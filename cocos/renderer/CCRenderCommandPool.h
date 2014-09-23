cff7df9a58291c7716a79e4591a36587783c7e64
Huabing.Xu <dabingnn@gmail.com>
2014-09-23 14:51:41 +0800

    add freePool class for commandPool

--- cocos/renderer/CCRenderCommandPool.h
+++ cocos/renderer/CCRenderCommandPool.h
@@ -29,8 +29,57 @@
 #include <list>
 
 #include "platform/CCPlatformMacros.h"
+#include "base/ccCArray.h"
 
 NS_CC_BEGIN
+
+template <class T>
+class FreePool
+{
+public:
+    FreePool()
+    {
+        static const size_t INTERNAL_CAPACITY = 128;
+        _array = (T**)malloc(sizeof(T*) * INTERNAL_CAPACITY);
+        _capacity = INTERNAL_CAPACITY;
+        _count = 0;
+    }
+    ~FreePool()
+    {
+        free(_array);
+    }
+    
+    T* get()
+    {
+        if(0 == _count) return nullptr;
+        return _array[--_count];
+    }
+    
+    void push(T* ptr)
+    {
+        if(_count + 1 == _capacity)
+        {
+            _capacity = _capacity * 2;
+            _array = (T**)realloc(_array, sizeof(T*) * _capacity);
+        }
+        _array[_count++] = ptr;
+    }
+    
+    size_t count() const
+    {
+        return _count;
+    }
+    
+    bool empty() const
+    {
+        return 0 == _count;
+    }
+    
+private:
+    T** _array;
+    size_t _capacity;
+    size_t _count;
+};
 
 template <class T>
 class RenderCommandPool
@@ -44,38 +93,35 @@ public:
         {
             AllocateCommands();
         }
-        result = _freePool.front();
-        _freePool.pop_front();
+        result = _freePool.get();
         result->setInPool();
         return result;
     }
     
     static void pushBackCommand(T* ptr)
     {
-        _freePool.push_front(ptr);
+        _freePool.push(ptr);
         
     }
 private:
     static void AllocateCommands()
     {
-        static const int COMMANDS_ALLOCATE_BLOCK_SIZE = 128;
+        static const int  COMMANDS_ALLOCATE_BLOCK_SIZE= 128;
         _allocatedPoolBlocks.push_back(std::vector<T>());
         _allocatedPoolBlocks.back().resize(COMMANDS_ALLOCATE_BLOCK_SIZE);
         auto& commands = _allocatedPoolBlocks.back();
         for(int index = 0; index < COMMANDS_ALLOCATE_BLOCK_SIZE; ++index)
         {
-            _freePool.push_back(&commands[index]);
+            _freePool.push(&commands[index]);
         }
     }
 
     static std::list<std::vector<T>> _allocatedPoolBlocks;
-    static std::list<T*> _freePool;
+    static FreePool<T> _freePool;
 };
 template <class T>
 std::list<std::vector<T>> RenderCommandPool<T>::_allocatedPoolBlocks;
-template <class T>
-std::list<T*> RenderCommandPool<T>::_freePool;
-
+template <class T> FreePool<T> RenderCommandPool<T>::_freePool;
 NS_CC_END
 
 #endif
